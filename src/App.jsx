b4f4a0fe8da048b372ca2ab6fc46a9cf91968f34
Satyam Rana <satyamranatc@gmail.com>
2026-06-16 11:13:27 +0530

    Bt Update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
       <center>
         <h1>Hello By Home</h1>
         <p>Hi</p>
-        <button>Bye</button>
+        <button onClick={() => alert('Hello')}>Click Me</button>
       </center>
     </div>
   )
